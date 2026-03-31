ad387d74c0b602d95deac6a8e8e1be2a1b2d7291
riina <54872398+riina@users.noreply.github.com>
2026-03-31 00:55:16 -0700

    fixup samples

--- samples/EdelweissTgp/EdelweissTgpProcessor.cs
+++ samples/EdelweissTgp/EdelweissTgpProcessor.cs
@@ -27,6 +27,13 @@ public class EdelweissTgpProcessor : FormatMultiProcessor
         };
     }
 
+    private enum Mode
+    {
+        Uncompressed,
+        SingleSolidCompress,
+        PerFileCompress
+    }
+
     private IEnumerable<Data> Process_0x0003()
     {
         ushort flags = u2l[0x6];
@@ -34,25 +41,60 @@ public class EdelweissTgpProcessor : FormatMultiProcessor
         int mainOff = 0x10 + count * 0x70;
         bool compress = (flags & 0x2) != 0;
         List<int>? positions = null;
+        if (Debug)
+        {
+            LogInfo(Name);
+        }
+        int endAll = (int)InputLength;
+        byte[]? decompressedSource = null;
+        Mode mode;
         if (compress)
         {
             positions = new List<int>(Match(mainOff, "\x28\xb5\x2f\xfd").Select(v => (int)v));
             if (positions.Count != count)
             {
-                LogWarn("Incorrect match count for zstd magic");
-                yield break;
+                if (positions.Count == 1 && count >= 1)
+                {
+                    int offset = positions[0];
+                    decompressedSource = new ZstandardStream(buf[offset, (int)InputLength - offset].Stream(), CompressionMode.Decompress).Dump();
+                    endAll = decompressedSource.Length;
+                    mainOff = 0;
+                    positions = null;
+                    compress = false;
+                    mode = Mode.SingleSolidCompress;
+                }
+                else
+                {
+                    LogWarn($"Incorrect match count for zstd magic {positions.Count}, expected {count}");
+                    yield break;
+                }
+            }
+            else
+            {
+                mode = Mode.PerFileCompress;
             }
         }
-
+        else
+        {
+            mode = Mode.Uncompressed;
+        }
+        if (Debug)
+        {
+            LogInfo($"{Name}: {mode}");
+        }
         for (int i = 0; i < count; i++)
         {
             byte[] entry = buf[0x10 + i * 0x70, 0x70];
-            string name = utf8[entry, 0x60].String;
+            string name = utf8[entry, 0, 0x60].String;
             int offset = positions?[i] ?? mainOff + i4l[entry, 0x60];
             int uncompressedLength = i4l[entry, 0x64];
-            int end = i + 1 == count ? (int)InputLength : positions?[i + 1] ?? offset + uncompressedLength;
-            if (Debug) LogInfo($"{offset:X8}/{uncompressedLength:X8} {name}");
-            var stream = new ZstandardStream(buf[offset, end - offset].Stream(), CompressionMode.Decompress);
+            int end = i + 1 == count ? endAll : positions?[i + 1] ?? offset + uncompressedLength;
+            if (Debug) LogInfo($"#{i} {offset:X8}..{offset + uncompressedLength:X8}({uncompressedLength:X8})/{endAll:X8} {name}");
+            var stream = compress
+                ? new ZstandardStream(buf[offset, end - offset].Stream(), CompressionMode.Decompress)
+                : decompressedSource != null
+                    ? buf[decompressedSource, offset, end - offset].ToArray().Stream()
+                    : buf[offset, end - offset].Stream();
             yield return Buffer(name, stream.Dump());
         }
     }
